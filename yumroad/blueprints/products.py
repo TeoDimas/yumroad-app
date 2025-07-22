0c4c4c798ef5c8d37444b4f880acfc3690732fa3
MASTER\TeoDimas <thdimasmail@gmail.com>
2025-07-22 10:46:08 +0300

    fix product creation to store price in cents and update picture_url field

--- yumroad/blueprints/products.py
+++ yumroad/blueprints/products.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, redirect, request, url_for, abort
+from flask import Blueprint, render_template, redirect, request, url_for, abort, flash
 from flask_login import login_required, current_user
+
+from yumroad.extensions import checkout
 
 from yumroad.models import Product, db
 from yumroad.forms import ProductForm
@@ -16,8 +18,11 @@ def index():
 def create():
     form = ProductForm()
     if form.validate_on_submit():
-        product = Product(name=request.form['name'],
-                        description=request.form['description'],
+        price = form.price.data or 0
+        product = Product(name=form.name.data,
+                        description=form.description.data,
+                        price_cents=int(price*100),
+                        picture_url=form.picture_url.data,
                         creator=current_user,
                         store=current_user.store)
         db.session.add(product)
@@ -28,19 +33,23 @@ def create():
 
 @product_bp.route('/product/<product_id>')
 def details(product_id):
-    product = Product.query.get(product_id)
-    if not product:
-        abort(404)
-    # or simply:
-    # product = Product.query.get_or_404(product_id)
-    return render_template('products/details.html', product=product)
+    product = Product.query.get_or_404(product_id)
+    stripe_publishable_key = checkout.publishable_key
+    stripe_session = checkout.create_session(product) or {}
+    session_id = stripe_session.get('id')
 
+    return render_template('products/details.html',
+                            product=product,
+                            stripe_publishable_key=stripe_publishable_key,
+                            checkout_session_id=session_id)
 
-# def plain_old_form_handling():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         description = request.form['description']
-
-#         product = Product(name=name, description=description)
-#         db.session.add(product)
-#         return redirect(url_for('.details', product_id=product.id))
+@product_bp.route('/product/<product_id>/post_checkout')
+def post_checkout(product_id):
+    product = Product.query.get_or_404(product_id)
+    purchase_state = request.args.get('status')
+    post_purchase_session_id = request.args.get('session_id')
+    if purchase_state == 'success' and post_purchase_session_id:
+        flash("Thanks for purchasing {}. You will receive an email shortly".format(product.name), 'success')
+    elif purchase_state == 'cancel' and post_purchase_session_id:
+        flash("There was an error while attempting to purchase this product. Try again", 'danger')
+    return redirect(url_for('.details', product_id=product_id))
