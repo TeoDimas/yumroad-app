47031d008ac2d565d9ec6775e7381f1073fac432
MASTER\TeoDimas <thdimasmail@gmail.com>
2025-07-22 08:09:46 +0300

    add SignupForm with email uniqueness and password confirmation validation

--- yumroad/forms.py
+++ yumroad/forms.py
@@ -1,12 +1,17 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, validators
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from wtforms.fields import StringField, SubmitField
-from wtforms.validators import Length
+from yumroad.models import User
+
+class EmptyForm(FlaskForm):
+    pass
 
 class ProductForm(FlaskForm):
-    name = StringField('Name', [Length(min=4, max=60)])
+    name = StringField('Name', [validators.Length(min=4, max=60)])
     description = StringField('Description')
-    submit = SubmitField('Create')class LoginForm(FlaskForm):
+
+class LoginForm(FlaskForm):
     email = StringField('Email', validators=[validators.Email(), validators.DataRequired()])
     password = PasswordField('Password', validators=[validators.DataRequired()])
 
@@ -22,5 +27,25 @@ class ProductForm(FlaskForm):
 
         if not check_password_hash(user.password, self.password.data):
             self.email.errors.append('Invalid email or password')
+            return False
+        return True
+
+class SignupForm(FlaskForm):
+    email = StringField('Email', validators=[validators.Email(), validators.DataRequired()])
+    password = PasswordField('Password', validators=[
+        validators.DataRequired(),
+        validators.Length(min=4),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Confirm Password', validators=[validators.DataRequired()])
+
+    def validate(self, extra_validators=None):
+        check_validate = super(SignupForm, self).validate()
+        if not check_validate:
+            return False
+
+        user = User.query.filter_by(email=self.email.data).first()
+        if user:
+            self.email.errors.append('That email already has an account')
             return False
         return True
