8ea994a071e1cdb6abf12765ce359e7609efa908
MASTER\TeoDimas <thdimasmail@gmail.com>
2025-07-22 08:14:19 +0300

    rename blueprint from @products to @product_bp for all product routes

--- yumroad/blueprints/products.py
+++ yumroad/blueprints/products.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, render_template, redirect, request, url_for, abort
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required
 
-from yumroad.models import Product, db
 from yumroad.forms import ProductForm
-products = Blueprint('products', __name__)
+from yumroad.models import Product, db
 
-@products.route('/')
+product_bp = Blueprint('product', __name__)
+
+@product_bp.route('/')
 def index():
     all_products = Product.query.all()
     return render_template('products/index.html', products=all_products)
 
-@products.route('/create', methods=['GET', 'POST'])
+@product_bp.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
     form = ProductForm()
     if form.validate_on_submit():
@@ -19,30 +22,7 @@ def create():
         return redirect(url_for('.details', product_id=product.id))
     return render_template('products/new.html', form=form)
 
-@products.route('/<product_id>')
+@product_bp.route('/<product_id>')
 def details(product_id):
     product = Product.query.get_or_404(product_id)
     return render_template('products/details.html', product=product)
-
-@products.route('/<product_id>/edit', methods=['GET', 'POST'])
-def edit(product_id):
-    product = Product.query.get_or_404(product_id)
-    form = ProductForm(obj=product)
-    if form.validate_on_submit():
-        product.name = form.name.data
-        product.description = form.description.data
-        db.session.add(product)
-        db.session.commit()
-        return redirect(url_for('.details', product_id=product.id))
-    return render_template('products/edit.html', form=form, product=product)
-
-# @products.route('/new', methods=['GET', 'POST'])
-# def plain_old_form_handling():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         description = request.form['description']
-
-#         product = Product(name=name, description=description)
-#         db.session.add(product)
-#         return redirect(url_for('.details', product_id=product.id))
-#     return render_template('products/new_plain_form.html')
