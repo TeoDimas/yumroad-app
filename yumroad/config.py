cbaee59774b55960ce2f61c5ef506fd8e1062cf6
MASTER\TeoDimas <thdimasmail@gmail.com>
2025-07-22 10:46:49 +0300

    add test Stripe webhook key to configuration

--- yumroad/config.py
+++ yumroad/config.py
@@ -13,6 +13,9 @@ class BaseConfig:
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', True)
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
+    STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY'] #os.getenv('STRIPE_SECRET_KEY', 'sk_test_k1')
+    STRIPE_PUBLISHABLE_KEY = os.environ['STRIPE_PUBLISHABLE_KEY'] #os.getenv('STRIPE_PUBLISHABLE_KEY', 'sk_test_k1')
+    STRIPE_WEBHOOK_KEY = os.getenv('STRIPE_WEBHOOK_KEY', 'whsec_test_secret')
 
 class DevConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(folder_path, 'dev.db'))
@@ -24,6 +27,7 @@ class TestConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(folder_path, 'test.db'))
     SECRET_KEY = os.getenv('YUMROAD_SECRET_KEY', '12345abcdef')
     WTF_CSRF_ENABLED = False
+    STRIPE_WEBHOOK_KEY = 'whsec_test_secret'
 
 class ProdConfig(BaseConfig):
     DEBUG = False
