b5a016864cf891e5b43315b02fff2c7ccde98184
MASTER\TeoDimas <thdimasmail@gmail.com>
2025-07-22 20:47:06 +0300

    integrate Sentry and custom error handlers for 401, 404, and 500

--- yumroad/__init__.py
+++ yumroad/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 from webassets.loaders import PythonLoader as PythonAssetsLoader
 
 from yumroad import assets
@@ -27,6 +30,25 @@ def create_app(environment_name='dev'):
     login_manager.init_app(app)
     checkout.init_app(app)
     assets_env.init_app(app)
+
+    if app.config.get("SENTRY_DSN"):
+        sentry_sdk.init(
+            dsn=app.config["SENTRY_DSN"],
+            send_default_pii=True,
+            integrations=[FlaskIntegration(), SqlalchemyIntegration()]
+        )
+
+    @app.errorhandler(401)
+    def unauthorized_error(error):
+        return render_template('errors/401.html'), 401 # pragma: no cover
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        return render_template('errors/500.html'), 500  # pragma: no cover
 
     assets_loader = PythonAssetsLoader(assets)
     for name, bundle in assets_loader.load_bundles().items():
