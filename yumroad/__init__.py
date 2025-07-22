a943711c97480252bd92d2d1403a6324c58ec739
MASTER\TeoDimas <thdimasmail@gmail.com>
2025-07-22 07:42:02 +0300

    import db and csrf extensions; comment out CSRF app initialization

--- yumroad/__init__.py
+++ yumroad/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask, render_template
 
 from yumroad.blueprints.products import products
 from yumroad.config import configurations
-from yumroad.extensions import (db)
+from yumroad.extensions import (db, csrf)
 
 
 def create_app(environment_name='dev'):
     app = Flask(__name__)
     app.config.from_object(configurations[environment_name])
     db.init_app(app)
+    # csrf.init_app(app)
     app.register_blueprint(products, url_prefix="/product")
     return app
 
-# FLASK_DEBUG=true FLASK_APP="yumroad:create_app" flask run
+# FLASK_ENV=development FLASK_APP="yumroad:create_app('dev')" flask run
