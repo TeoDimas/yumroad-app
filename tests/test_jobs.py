3e6e082f4c5f92ed3b85fc5632fda6ba186160d4
MASTER\TeoDimas <thdimasmail@gmail.com>
2025-07-23 06:16:40 +0300

    replace jobs_overview route with overview route in client requests

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -3,12 +3,12 @@ import pytest
 
 # Functional Tests
 def test_get_rq_admin(client, init_database):
-    response = client.get(url_for('rq_dashboard.jobs_overview', instance_number=0))
+    response = client.get(url_for('rq_dashboard.overview'))
     assert response.status_code == 302
 
 def test_get_rq_admin_authed(client, init_database, authenticated_request):
     authed_user = authenticated_request
-    response = client.get(url_for('rq_dashboard.jobs_overview', instance_number=0))
+    response = client.get(url_for('rq_dashboard.overview'))
 
     assert response.status_code == 200
     assert b'RQ Instances' in response.data
